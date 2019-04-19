bce51e7f9ac643356ed3c46d1a47354968d71fa9
Karol Kotowicz <kkotowiczz@gmail.com>
2019-04-19 07:41:37 +0200

    exercise 3 ch 3

--- chapter3/chapter3.scala
+++ chapter3/chapter3.scala
@@ -4,13 +4,13 @@ import scala.util._
 //               (inclusive) and n (exclusive).
 
 def generateRandomArray(n: Int): Array[Int] = {
-    val r = new Random
-    val seq = for(i <- 0 to n) yield r.nextInt(n)
-    seq.toArray
+  val r = new Random
+  val seq = for(i <- 0 to n) yield r.nextInt(n)
+  seq.toArray
 }
 
 val arr1 = generateRandomArray(35)
-arr1.foreach(println)
+//arr1.foreach(println)
 
 // Exercise 2: Write a loop that swaps adjacent elements of an array of integers. For example,
 //             Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5) .
@@ -19,7 +19,14 @@ val arr2 = Array(1, 2, 3, 4, 5)
 
 for(i <- 0 until arr2.length if i % 2 == 1) { val temp = arr2(i); arr2(i) = arr2(i - 1); arr2(i - 1) = temp }
 
-arr2.foreach(println)
+//arr2.foreach(println)
 
 val arr3 = Array(1, 2, 3, 4, 5)
 
+val arr4 = for {
+  i <- 0 until arr3.length
+  if(i % 2 == 1)
+  j <- (i to i - 1 by -1)
+} yield arr3(j)
+
+arr4.foreach(println)
