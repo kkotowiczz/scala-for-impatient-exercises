ad76a8192cfdff419f8982b697a4b1a367da3783
Karol Kotowicz <kkotowiczz@gmail.com>
2019-04-28 14:37:31 +0200

    testing solution in main method

--- src/main/scala/com/github/kkotowiczz/chapter8/chapter8.scala
+++ src/main/scala/com/github/kkotowiczz/chapter8/chapter8.scala
@@ -1,9 +1,15 @@
 package com.github.kkotowiczz.chapter8
 
-import com.github.kkotowiczz.chapter8.items.SimpleItem
+import com.github.kkotowiczz.chapter8.items.{Bundle, SimpleItem}
 
 object chapter8 extends App {
   val si = new SimpleItem(356.12, "some item")
 
   println(si.description)
+
+  val item1 = new SimpleItem(23.3, "some other Item")
+
+  val bundle = new Bundle(si :: item1 :: Nil)
+
+  println(bundle.price, bundle.description)
 }
